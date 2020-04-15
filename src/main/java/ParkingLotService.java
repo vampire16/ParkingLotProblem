8ff3c1a4172e8d2c03552a5192c6439d27e6b68e
vampire16 <hakevaibhav16@gmail.com>
2020-04-15 09:07:46 +0530

    ADDED : Unpark method to unpark vehicle

--- src/main/java/ParkingLotService.java
+++ src/main/java/ParkingLotService.java
@@ -1,5 +1,18 @@
 public class ParkingLotService {
+    private Object vehicle;
+
     public boolean park(Object vehicle) {
+        if(this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean unPark(Object vehicle) {
+        if(this.vehicle == vehicle){
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
