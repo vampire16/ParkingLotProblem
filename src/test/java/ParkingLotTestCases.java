e8c305c5ac86076a3ff60ca1681784be347bd54e
vampire16 <hakevaibhav16@gmail.com>
2020-04-15 09:09:36 +0530

    New test methods to check unparked vehicle

--- src/test/java/ParkingLotTestCases.java
+++ src/test/java/ParkingLotTestCases.java
@@ -13,8 +13,29 @@ public class ParkingLotTestCases {
     }
 
     @Test
-    public void givenVehicle_WhenPark_ShouldReturnTrue() {
-        boolean isPark = parkingLotService.park(vehicle);
-        Assert.assertTrue(isPark);
+    public void givenVehicle_WhenParked_ShouldReturnTrue() {
+        boolean isParked = parkingLotService.park(vehicle);
+        Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingLotService.park(vehicle);
+        boolean isParked = parkingLotService.park(new Object());
+        Assert.assertFalse(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLotService.park(vehicle);
+        boolean isUnParked = parkingLotService.unPark(vehicle);
+        Assert.assertTrue(isUnParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParkedWhichIsNotParked_ShouldReturnFalse() {
+        parkingLotService.park(vehicle);
+        boolean isUnParked = parkingLotService.unPark(new Object());
+        Assert.assertFalse(isUnParked);
     }
 }
