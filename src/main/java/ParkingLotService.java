759c0cc220af532b78a5845a79e4f1f68fd84799
vampire16 <hakevaibhav16@gmail.com>
2020-04-15 08:12:21 +0530

    ADDED : Park method to park vehicle

--- src/main/java/ParkingLotService.java
+++ src/main/java/ParkingLotService.java
@@ -1,5 +1,5 @@
 public class ParkingLotService {
-    public static void main(String[] args){
-        System.out.println("Welcome");
+    public boolean park(Object vehicle) {
+        return true;
     }
 }
