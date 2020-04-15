2187bb609232000188d4e9683bac0133bcbeb385
vampire16 <hakevaibhav16@gmail.com>
2020-04-15 08:13:12 +0530

    ADDED : New test method to check vehicle is parked or not

--- src/test/java/ParkingLotTestCases.java
+++ src/test/java/ParkingLotTestCases.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ParkingLotTestCases {
+    Object vehicle = null;
+    ParkingLotService parkingLotService = null;
+
+    @Before
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingLotService = new ParkingLotService();
+    }
+
+    @Test
+    public void givenVehicle_WhenPark_ShouldReturnTrue() {
+        boolean isPark = parkingLotService.park(vehicle);
+        Assert.assertTrue(isPark);
+    }
 }
