ebee14bd13d74c9b1253ada9ba7a864332dcf5eb
vampire16 <hakevaibhav16@gmail.com>
2020-04-17 14:10:31 +0530

    ADDED : Throwing exception of parking lot is empty

--- src/main/java/ParkingLotService.java
+++ src/main/java/ParkingLotService.java
@@ -2,30 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingLotService {
-    private Object vehicle;
     public int parkingCapacity;
-    public List list=new ArrayList();
+    public List list = new ArrayList();
 
     public ParkingLotService(int parkingCapacity) {
         this.parkingCapacity = parkingCapacity;
     }
 
-    public boolean park(Object vehicle) {
+    public boolean park(Object vehicle) throws ParkingLotException {
         if (list.size() == this.parkingCapacity) {
             Owner.ParkingLotInfo = "Parking lot is full";
             AirportSecurity.ParkingLotInfo = "Parking lot is full";
-        } else {
-            list.add(vehicle);
-            return true;
+            throw new ParkingLotException("Parking lot is full");
         }
-        return false;
+        list.add(vehicle);
+        return true;
+
     }
 
-    public boolean unPark(Object vehicle) {
+    public boolean unPark(Object vehicle) throws ParkingLotException {
         if (list.contains(vehicle)) {
             list.remove(vehicle);
-            if (list.size() == 0)
+            if (list.size() == 0) {
                 Owner.ParkingLotInfo = "Parking lot is empty";
+                throw new ParkingLotException("Parking lot is empty");
+            }
             return true;
         }
         return false;
