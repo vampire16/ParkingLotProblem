f75656e09554f47359e22af0e524d355a3d967a2
vampire16 <hakevaibhav16@gmail.com>
2020-04-27 17:35:17 +0530

    ADDED : New method to find vehicles parking slot number

--- src/main/java/com/bridgelabz/Utility/ParkingAttendant.java
+++ src/main/java/com/bridgelabz/Utility/ParkingAttendant.java
@@ -3,8 +3,15 @@ package com.bridgelabz.Utility;
 import java.util.HashMap;
 
 public class ParkingAttendant {
-    HashMap<Integer, Object> lotMap;
+    HashMap<Integer, Object> lotMap = new HashMap<>();
     Owner owner = new Owner();
+
+    public Integer getMyParkingSlot(Object vehicle){
+        for (int i = 1; i <=lotMap.size(); i++)
+            if (lotMap.get(i) == vehicle)
+                return i;
+        return null;
+    }
 
     public HashMap<Integer, Object> parkVehicle(Object vehicle,HashMap<Integer, Object> lotMap){
         this.lotMap = lotMap;
